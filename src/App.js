9325f399ef6f4d17b95db54698a8afa7d96485f6
Diego Sigcho Alvarado <sigchodiego@gmail.com>
2024-03-25 13:09:48 -0500

    Create project Pokedux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { Col } from "antd";
+import { Searcher } from './Components/Searcher';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Col span={8} offset={8} >
+        <Searcher />
+      </Col>
     </div>
   );
 }
